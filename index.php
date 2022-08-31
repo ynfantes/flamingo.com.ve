a715038bf8fca7a1b232734ce3dde6f34ae93a4d
Edgar Messia <ynfantes@gmail.com>
2022-08-31 17:44:30 +0200

    Fix HTTP_ACCEPT_ENCODING undefined index

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
-if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start();
 include_once 'includes/constants.php';
 echo $twig->render('login.html.twig');
